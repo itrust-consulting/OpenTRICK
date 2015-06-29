0217e58c080db2b36d71a60de8952ede521cf2b5
eomar <eomar@Eomar-Envy>
2015-06-29 16:22:20 +0200

    Separating export

---
+++ src/main/webapp/WEB-INF/js/trickservice/analysisExport.js
@@ -0,0 +1,72 @@
+function exportAnalysis(analysisId) {
+	if (analysisId == null || analysisId == undefined) {
+		var selectedScenario = findSelectItemIdBySection("section_analysis");
+		if (selectedScenario.length != 1)
+			return false;
+		analysisId = selectedScenario[0];
+	}
+	if (userCan(analysisId, ANALYSIS_RIGHT.EXPORT)) {
+		$.ajax({
+			url : context + "/Analysis/Export/" + analysisId,
+			type : "get",
+			async : true,
+			contentType : "application/json;charset=UTF-8",
+			success : function(response, textStatus, jqXHR) {
+				if (response["success"] != undefined) {
+					application["taskManager"].Start();
+				} else if (response["error"] != undefined) {
+					$("#alert-dialog .modal-body").html(response["error"]);
+					$("#alert-dialog").modal("toggle");
+				} else
+					unknowError();
+			},
+			error : unknowError
+		});
+	} else
+		permissionError();
+	return false;
+}
+
+function exportAnalysisReport(analysisId) {
+	if (analysisId == null || analysisId == undefined) {
+		var selectedScenario = findSelectItemIdBySection("section_analysis");
+		if (selectedScenario.length != 1)
+			return false;
+		analysisId = selectedScenario[0];
+	}
+	if (userCan(analysisId, ANALYSIS_RIGHT.EXPORT)) {
+		$.ajax({
+			url : context + "/Analysis/Export/Report/" + analysisId,
+			type : "get",
+			async : true,
+			contentType : "application/json;charset=UTF-8",
+			success : function(response, textStatus, jqXHR) {
+				if (response["success"] != undefined)
+					application["taskManager"].Start();
+				else if (response["error"] != undefined) {
+					$("#alert-dialog .modal-body").html(response["error"]);
+					$("#alert-dialog").modal("toggle");
+				} else
+					unknowError();
+			},
+			error : unknowError
+		});
+	} else
+		permissionError();
+	return false;
+}
+
+function exportAnalysisReportData(analysisId) {
+	if (analysisId == null || analysisId == undefined) {
+		var selectedScenario = findSelectItemIdBySection("section_analysis");
+		if (selectedScenario.length != 1)
+			return false;
+		analysisId = selectedScenario[0];
+	}
+	if (userCan(analysisId, ANALYSIS_RIGHT.EXPORT)) {
+		$.fileDownload(context + '/Analysis/Export/ReportData/' + analysisId).fail(unknowError);
+		return false;
+	} else
+		permissionError();
+	return false;
+}
