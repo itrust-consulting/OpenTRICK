2d2b01f34b600be4ce092709831c20196d3840b4
eomar <eomar@Eomar-Envy>
2015-09-25 15:58:44 +0200

    Prepare profile test

---
+++ src/test/java/lu/itrust/TS/controller/TS_07_Profile.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package lu.itrust.TS.controller;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author eomar
+ *
+ */
+@Test(groups = "Profile", dependsOnGroups = "Installation")
+public class TS_07_Profile extends SpringTestConfiguration  {
+
+	@Test
+	public void test_00_UpdateProfile(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_01_UpdateUsername(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_01_UpdateConnexionType(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_01_UpdateRole(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_02_UpdateSQLITEFilter(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_03_LoadSQLITEUI() {
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_04_DeleteSQLITE() {
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	@Test
+	public void test_05_UpdateReportFilter(){
+		throw new IllegalArgumentException("Test not implemented");
+	}
+	
+	
+}
