edc263d29888535b0515e6b80da11a1dc9323e04
eomar <omar@itrust.lu>
2017-02-26 09:33:13 +0100

    migrate database.

---
+++ src/main/java/lu/itrust/business/TS/database/migration/V2_2__1_MigrateAssessment.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package lu.itrust.business.TS.database.migration;
+
+import org.flywaydb.core.api.MigrationVersion;
+import org.flywaydb.core.api.migration.MigrationInfoProvider;
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author eomar
+ *
+ */
+public class V2_2__1_MigrateAssessment implements SpringJdbcMigration, MigrationInfoProvider  {
+	/**
+	 * 
+	 */
+	public V2_2__1_MigrateAssessment() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.flywaydb.core.api.migration.spring.SpringJdbcMigration#migrate(org.springframework.jdbc.core.JdbcTemplate)
+	 */
+	@Override
+	public void migrate(JdbcTemplate arg0) throws Exception {
+		System.out.println("Parameters");
+	}
+
+	@Override
+	public String getDescription() {
+		return "Migrate assessment impact";
+	}
+
+	@Override
+	public MigrationVersion getVersion() {
+		return MigrationVersion.fromVersion("2.2");
+	}
+
+}
