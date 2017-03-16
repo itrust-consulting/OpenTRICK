5e25f29b724ae0edca3f69e08f7dba968e380b2d
eomar <omar@itrust.lu>
2017-03-16 17:30:54 +0100

    fix bug edit analysis (details)

--- src/main/webapp/WEB-INF/js/trickservice/knowledgebase.js
+++ src/main/webapp/WEB-INF/js/trickservice/knowledgebase.js
@@ -29,8 +29,45 @@ function editSingleAnalysis(analysisId) {
 			var $view = $("#editAnalysisModel",new DOMParser().parseFromString(response, "text/html"));
 			if (!$view.length) {
 				showDialog("#alert-dialog",MessageResolver("error.unknown.data.loading", "An unknown error occurred during data loading"));
-			} else 
+			} else {
 				$view.appendTo("#widget").modal('show').on("hidden.bs.modal", () => $view.remove());
+				$("button[name='save']", $view).on("click", e => {
+					$progress.show();
+					$(".label-danger", $view).remove();
+					$.ajax({
+						url: context + "/Analysis/Save",
+						type: "post",
+						data: serializeForm($("form", $view)),
+						contentType: "application/json;charset=UTF-8",
+						success: function (response, textStatus, jqXHR) {
+							var hasError = false;
+							for (var error in response) {
+								var $errorElement = $("<label class='label label-danger' />").text(response[error]);
+								switch (error) {
+									case "analysislanguage":
+										$errorElement.appendTo($("#analysislanguagecontainer", $view));
+										break;
+									case "comment":
+										$errorElement.appendTo($("input[name='comment']", $view).parent());
+										break;
+									default:
+										showDialog("#alert-dialog", response[error]);
+										break;
+								}
+								hasError = true;
+							}
+							if (!hasError) {
+								$view.modal("hide");
+								reloadSection("section_profile_analysis");
+							}
+						},
+						error: unknowError
+					}).complete(function () {
+						$progress.hide();
+					});
+					
+				});
+			}
 		},
 		error: unknowError
 	}).complete(function () {
@@ -99,45 +136,6 @@ function selectAnalysis(analysisId) {
 	}
 	$("#loading-indicator").show();
 	setTimeout(() =>window.location.replace(context + "/Analysis/" + analysisId + "/Select?open=" + OPEN_MODE.EDIT.value), 0);
-	return false;
-}
-
-function saveAnalysis(form, reloadaction) {
-	var $progress = $("#loading-indicator").show();
-	$("#editAnalysisModel .label-danger").remove();
-	$.ajax({
-		url: context + "/Analysis/Save",
-		type: "post",
-		data: serializeForm(form),
-		contentType: "application/json;charset=UTF-8",
-		success: function (response, textStatus, jqXHR) {
-			var hasError = false;
-			for (var error in response) {
-				var errorElement = document.createElement("label");
-				errorElement.setAttribute("class", "label label-danger");
-				$(errorElement).text(response[error]);
-				switch (error) {
-					case "analysislanguage":
-						$(errorElement).appendTo($("#analysislanguagecontainer"));
-						break;
-					case "comment":
-						$(errorElement).appendTo($("#analysis_label").parent());
-						break;
-					default:
-						showDialog("#alert-dialog", response["error"]);
-						break;
-				}
-				hasError = true;
-			}
-			if (!hasError) {
-				$("#editAnalysisModel").modal("hide");
-				reloadSection("section_profile_analysis");
-			}
-		},
-		error: unknowError
-	}).complete(function () {
-		$progress.hide();
-	});
 	return false;
 }
 
