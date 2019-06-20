32696bf562a4a26a55e12d6ebc1de25ba8b6ec88
Ensuifudine OMAR <omar@itrust.lu>
2019-06-20 19:05:33 +0200

    update config.

---
+++ src/main/java/lu/itrust/business/boot/Application.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package lu.itrust.business.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author eomar
+ *
+ */
+@SpringBootApplication(exclude = { FlywayAutoConfiguration.class, DataSourceAutoConfiguration.class,
+		FreeMarkerAutoConfiguration.class , SecurityAutoConfiguration.class})
+@ImportResource("classpath:spring/application-config.xml")
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/lu/itrust/business/boot/Configuration.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lu.itrust.business.boot;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * @author eomar
+ *
+ */
+
+public class Configuration {
+
+}
