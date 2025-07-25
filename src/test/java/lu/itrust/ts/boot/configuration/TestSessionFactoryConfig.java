a5da1b6f098637d20c0bae473393f2c27ad81341
Ensuifudine OMAR <omar@itrust.lu>
2025-07-25 20:35:38 +0200

    update test config

--- src/test/java/lu/itrust/ts/boot/configuration/TestSessionFactoryConfig.java
+++ src/test/java/lu/itrust/ts/boot/configuration/TestSessionFactoryConfig.java
@@ -37,90 +37,78 @@ import lu.itrust.business.ts.usermanagement.helper.CustomerDaoAuthenticationProv
 @EnableTransactionManagement
 public class TestSessionFactoryConfig {
 
-    private static final String SELECT_USER_DT_LOGIN_AS_USERNAME_ROLE_DT_TYPE_FROM_USER_ROLE_USER_ROLE_WHERE_USER_ROLE_FI_USER_USER = "SELECT User.dtLogin as username, Role.dtType FROM UserRole, User, Role WHERE UserRole.fiUser = User.idUser AND UserRole.fiRole = Role.idRole AND User.dtLogin = ?";
-    private static final String SELECT_DT_LOGIN_AS_USERNAME_DT_PASSWORD_AS_PASSWORD_DT_ENABLED_AS_ENABLE_FROM_USER_WHERE_1_DT_CONNEX = "SELECT dtLogin as username, dtPassword as password, dtEnabled as enable from User where 1> dtConnexionType and dtLogin=?";
+        private static final String SELECT_USER_DT_LOGIN_AS_USERNAME_ROLE_DT_TYPE_FROM_USER_ROLE_USER_ROLE_WHERE_USER_ROLE_FI_USER_USER = "SELECT User.dtLogin as username, Role.dtType FROM UserRole, User, Role WHERE UserRole.fiUser = User.idUser AND UserRole.fiRole = Role.idRole AND User.dtLogin = ?";
+        private static final String SELECT_DT_LOGIN_AS_USERNAME_DT_PASSWORD_AS_PASSWORD_DT_ENABLED_AS_ENABLE_FROM_USER_WHERE_1_DT_CONNEX = "SELECT dtLogin as username, dtPassword as password, dtEnabled as enable from User where 1> dtConnexionType and dtLogin=?";
 
-    @Autowired
-    private DataSource dataSource;
+        @Autowired
+        private DataSource dataSource;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+        @Autowired
+        private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private @Lazy AuthenticationManager authenticationManager;
+        @Autowired
+        private @Lazy AuthenticationManager authenticationManager;
 
-    @Order(1)
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http)
-            throws Exception {
-        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        builder.authenticationProvider(customDaoAuthenticationProvider());
-        return builder.build();
-    }
+        @Order(1)
+        @Bean
+        public AuthenticationManager authenticationManager(HttpSecurity http)
+                        throws Exception {
+                var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+                builder.authenticationProvider(customDaoAuthenticationProvider());
+                return builder.build();
+        }
 
-    @Bean
-    @ConditionalOnMissingBean
-    public LocalSessionFactoryBean sessionFactory(DataSource firstDataSource, JpaProperties firstJpaProperties) {
-        final LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        final Properties properties = new Properties();
-        firstJpaProperties.getProperties().forEach(properties::put);
-        sessionFactoryBean.setDataSource(firstDataSource);
-        sessionFactoryBean.setPackagesToScan("lu.itrust.business.ts");
-        sessionFactoryBean.setHibernateProperties(properties);
-        return sessionFactoryBean;
-    }
+        @Bean
+        @ConditionalOnMissingBean
+        public LocalSessionFactoryBean sessionFactory(DataSource firstDataSource, JpaProperties firstJpaProperties) {
+                final LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+                final Properties properties = new Properties();
+                firstJpaProperties.getProperties().forEach(properties::put);
+                sessionFactoryBean.setDataSource(firstDataSource);
+                sessionFactoryBean.setPackagesToScan("lu.itrust.business.ts");
+                sessionFactoryBean.setHibernateProperties(properties);
+                return sessionFactoryBean;
+        }
 
-    @Bean
-    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
-    }
+        @Bean
+        public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
+                return new HibernateTransactionManager(sessionFactory);
+        }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("app.jpa.first")
-    public JpaProperties firstJpaProperties(Environment env, ResourceLoader resourceLoader) throws IOException {
-        final String path = "classpath:/persistence/ehcache-" + env.getProperty("jdbc.cache.storage.type")
-                + ".xml";
-        final JpaProperties properties = new JpaProperties();
-        final Resource resource = resourceLoader.getResource(path);
-        SessionFactoryConfig.loadSpringJpaProperties(env, properties.getProperties());
-        properties.getProperties().put("hibernate.dialect", env.getProperty("jdbc.dialect"));
-        properties.getProperties().put("hibernate.show_sql", env.getProperty("jdbc.show_sql"));
-        properties.getProperties().put("hibernate.jdbc.time_zone", env.getProperty("jdbc.time_zone"));
-        properties.getProperties().put("hibernate.Impl2ddl.auto", env.getProperty("jdbc.Impl2ddl.auto"));
-        properties.getProperties().put("hibernate.javax.cache.missing_cache_strategy",
-                env.getProperty("jdbc.cache.missing_cache_strategy"));
-        properties.getProperties().put("hibernate.javax.cache.provider",
-                env.getProperty("jdbc.cache.provider"));
-        properties.getProperties().put("hibernate.cache.use_query_cache",
-                env.getProperty("jdbc.cache.use_query_cache"));
-        properties.getProperties().put("hibernate.cache.region.factory_class",
-                env.getProperty("jdbc.cache.factory_class"));
-        properties.getProperties().put("hibernate.enable_lazy_load_no_trans", "true");
-        properties.getProperties().put("hibernate.cache.use_second_level_cache",
-                env.getProperty("jdbc.cache.use_second_level"));
-        if (resource.exists())
-            properties.getProperties().put("hibernate.javax.cache.uri", resource.getURI().toString());
-        return properties;
-    }
+        @Bean
+        @Primary
+        @ConfigurationProperties("app.jpa.first")
+        public JpaProperties firstJpaProperties(Environment env, ResourceLoader resourceLoader) throws IOException {
+                final String path = "classpath:/persistence/ehcache-"
+                                + env.getProperty("app.settings.hibernate.cache.second_level_cache.type")
+                                + ".xml";
+                final JpaProperties properties = new JpaProperties();
+                final Resource resource = resourceLoader.getResource(path);
+                SessionFactoryConfig.loadSpringJpaProperties(env, properties.getProperties());
+                if (resource.exists())
+                        properties.getProperties().put("hibernate.javax.cache.uri", resource.getURI().toString());
 
-    @Bean
-    public DaoAuthenticationProvider customDaoAuthenticationProvider() {
-        var dao = new CustomerDaoAuthenticationProvider();
-        dao.setUserDetailsService(userDetailsService(dataSource,authenticationManager));
-        dao.setPasswordEncoder(passwordEncoder);
-        return dao;
-    }
+                return properties;
+        }
 
-    @Bean
-    public UserDetailsManager userDetailsService(DataSource dataSource, AuthenticationManager authenticationManager) {
-        var userDetailsService = new JdbcUserDetailsManager(dataSource);
-        userDetailsService.setUsersByUsernameQuery(
-                SELECT_DT_LOGIN_AS_USERNAME_DT_PASSWORD_AS_PASSWORD_DT_ENABLED_AS_ENABLE_FROM_USER_WHERE_1_DT_CONNEX);
-        userDetailsService.setAuthoritiesByUsernameQuery(
-                SELECT_USER_DT_LOGIN_AS_USERNAME_ROLE_DT_TYPE_FROM_USER_ROLE_USER_ROLE_WHERE_USER_ROLE_FI_USER_USER);
-        userDetailsService.setAuthenticationManager(authenticationManager);
-        return userDetailsService;
-    }
+        @Bean
+        public DaoAuthenticationProvider customDaoAuthenticationProvider() {
+                var dao = new CustomerDaoAuthenticationProvider();
+                dao.setUserDetailsService(userDetailsService(dataSource, authenticationManager));
+                dao.setPasswordEncoder(passwordEncoder);
+                return dao;
+        }
+
+        @Bean
+        public UserDetailsManager userDetailsService(DataSource dataSource,
+                        AuthenticationManager authenticationManager) {
+                var userDetailsService = new JdbcUserDetailsManager(dataSource);
+                userDetailsService.setUsersByUsernameQuery(
+                                SELECT_DT_LOGIN_AS_USERNAME_DT_PASSWORD_AS_PASSWORD_DT_ENABLED_AS_ENABLE_FROM_USER_WHERE_1_DT_CONNEX);
+                userDetailsService.setAuthoritiesByUsernameQuery(
+                                SELECT_USER_DT_LOGIN_AS_USERNAME_ROLE_DT_TYPE_FROM_USER_ROLE_USER_ROLE_WHERE_USER_ROLE_FI_USER_USER);
+                userDetailsService.setAuthenticationManager(authenticationManager);
+                return userDetailsService;
+        }
 
 }
